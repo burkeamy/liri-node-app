647d0f65e434c6fd69afa8316b125d4a4e0e128f
unknown <burkeamy30@gmail.com>
2019-02-24 12:44:36 -0500

    the api calls are woring

--- liri.js
+++ liri.js
@@ -1,43 +1,71 @@
 
 require("dotenv").config()
-var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
-var axios = require('axios');
-var request = process.argv[2];
+const keys = require('./keys.js');
+const Spotify = require('node-spotify-api');
+const axios = require('axios');
+const request = process.argv[2];
+const requestSpecific = process.argv.slice(3).join(' ');
+const spotify = new Spotify(keys.spotify)
+const fs = require("fs");
 
-var spotify = new Spotify(keys.spotify)
-
+//This section is for the spotify song request
 if (request === 'spotify-this-song') { 
-  var song = process.argv[3];
-  spotify
-    .search({ type: 'track', query: song })
-   .then(function(response) {
-     console.log(response);
-  })
-    .catch(function(err) {
-    console.log(err);
+  let song = requestSpecific;
+  console.log('searcing for ' + song)
+  spotify.search({ type: 'track', query: song })
+   .then(function(err, response) {
+     if(err) {
+       console.log(err);
+     } else {
+     console.log(response.data);
+    }  
   });
 }
 
+//This section is for the omdb movie request
 if (request === 'movie-this') {
-  var movieName = process.argv[3];
-  if (movieName === ' ') {
-    axios.get("http://www.omdbapi.com/?t=Mr._Nobody&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("The movie's rating is: " + response.data.imdbRating);
-    } 
-  }) else {
+  let movieName = requestSpecific;
+  console.log("searching for " + movieName)
+    if (!movieName) {
+    axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
+    function(err, response) {
+      if(err) {
+        console.log(err); 
+        console.log(response);
+      } else {
       axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(
       function(err, response) {
-      console.log("The movie's rating is: " + response.data.imdbRating);
-  })
+        console.log(err);
+        console.log(response);
+       //console.log("The movie's rating is: " + response.data.imdbRating);
+      });
+    };
+  });
+  }
 }
 
+//this section is for the band's in town request
 if (request === 'concert-this') {
-  var artist = process.argv[3];
-  axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-  console.log(response);
-  })
-    .catch(function(err) {
-    console.log(err)
+  console.log('concert')
+  let artist = requestSpecific;
+  axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+    function(err, response) {
+      if(err){
+        console.log(err);
+      } else {
+      console.log(response.data);
+      }
+    });
+  }   
+
+//do what it says
+if (request === 'do-what-it-says') {
+    fs.readFile("random.txt", "utf8", function(err, response) {
+      if(err) {
+        console.log(err);
+      } else {
+        let responseArr = response.split(",");
+        console.log(responseArr);
+      }
+    });
 }
